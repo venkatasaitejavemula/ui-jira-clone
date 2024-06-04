66f302caf730f2258218d4e1be6121d32d1b9b06
venkatasaitejavemula <saiteja467402@gmail.com>
2024-06-04 22:02:52 +0530

    changes

--- src/JiraBoard.jsx
+++ src/JiraBoard.jsx
@@ -2,10 +2,10 @@ import { useCallback, useEffect, useState } from "react";
 import { getTicketsAPI, getUsersAPI, updateTicketAPI } from "./api's/CallAPI";
 import { TICKET_STATUS } from "./constants";
 import Section from "./components/Section";
-import NavigationBar from "./components/NavigationBar";
 import Header from "./components/Header";
 import { useDispatch, useSelector } from "react-redux";
 import { ticketSliceActions } from "./slice/ticketSlice";
+import { Modal } from "./components/Modal";
 
 function JiraBoard() {
   const [ticketDragged, setTicketDragged] = useState({});
@@ -114,52 +114,56 @@ function JiraBoard() {
     setTickets(reduxTickets);
   }, [reduxTickets]);
   return (
-    <div className="flex gap-6 h-full">
-      <div className="flex flex-col overflow-auto">
-        <Header
-          selectedUser={selectedUser}
-          onSelectUser={onSelectUser}
-          handleSearch={handleSearch}
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-        />
-        <br />
-        <div className="tickets-container ">
-          <Section
-            title={"TO DO"}
-            tickets={tickets}
-            filterStatus={TICKET_STATUS.TODO}
-            handleDrag={handleDrag}
-            handleDrop={handleDrop}
-            onDragOver={onDragOver}
+    <>
+      {/* <Modal /> */}
+
+      <div className="flex gap-6 h-full">
+        <div className="flex flex-col overflow-auto">
+          <Header
+            selectedUser={selectedUser}
+            onSelectUser={onSelectUser}
+            handleSearch={handleSearch}
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
           />
-          <Section
-            title={"IN PROGRESS"}
-            tickets={tickets}
-            filterStatus={TICKET_STATUS.IN_PROGRESS}
-            handleDrag={handleDrag}
-            handleDrop={handleDrop}
-            onDragOver={onDragOver}
-          />
-          <Section
-            title={"READY FOR TESTING"}
-            tickets={tickets}
-            filterStatus={TICKET_STATUS.READY_FOR_TESTING}
-            handleDrag={handleDrag}
-            handleDrop={handleDrop}
-            onDragOver={onDragOver}
-          />
-          <Section
-            title={"DONE"}
-            tickets={tickets}
-            filterStatus={TICKET_STATUS.DONE}
-            handleDrag={handleDrag}
-            handleDrop={handleDrop}
-            onDragOver={onDragOver}
-          />
+          <br />
+          <div className="tickets-container ">
+            <Section
+              title={"TO DO"}
+              tickets={tickets}
+              filterStatus={TICKET_STATUS.TODO}
+              handleDrag={handleDrag}
+              handleDrop={handleDrop}
+              onDragOver={onDragOver}
+            />
+            <Section
+              title={"IN PROGRESS"}
+              tickets={tickets}
+              filterStatus={TICKET_STATUS.IN_PROGRESS}
+              handleDrag={handleDrag}
+              handleDrop={handleDrop}
+              onDragOver={onDragOver}
+            />
+            <Section
+              title={"READY FOR TESTING"}
+              tickets={tickets}
+              filterStatus={TICKET_STATUS.READY_FOR_TESTING}
+              handleDrag={handleDrag}
+              handleDrop={handleDrop}
+              onDragOver={onDragOver}
+            />
+            <Section
+              title={"DONE"}
+              tickets={tickets}
+              filterStatus={TICKET_STATUS.DONE}
+              handleDrag={handleDrag}
+              handleDrop={handleDrop}
+              onDragOver={onDragOver}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Modal = ({ show = true, onClose, children }) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close" onClick={onClose}>
+          &times;
+        </button>
+        {"HI"}
+      </div>
+    </div>
+  );
+};
